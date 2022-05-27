6c32643f7a90879c70ef39d93724eef6330bd32d
rpoznanski <r.poznanski@teb-akademia.pl>
2022-05-27 15:31:40 +0200

    work Jogging

--- Jogging/app/src/main/java/com/example/jogging/MainActivity.kt
+++ Jogging/app/src/main/java/com/example/jogging/MainActivity.kt
@@ -5,8 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager.widget.ViewPager
 import com.example.jogging.models.Jogging
+import com.google.android.material.tabs.TabLayout
+
+private class SectionsPageAdapter : FragmentPagerAdapter() {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when(position){
+            0 -> HomeFragment()
+            else -> HomeFragment()
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity(), JoggingListFragment.Listener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +31,12 @@ class MainActivity : AppCompatActivity(), JoggingListFragment.Listener {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        val pager: ViewPager = findViewById(R.id.pager)
+
+        val tabLayout: TabLayout = findViewById(R.id.tabs)
+        tabLayout.setupWithViewPager(pager)
+
     }
 
     override fun itemClicked(id: Long) {
